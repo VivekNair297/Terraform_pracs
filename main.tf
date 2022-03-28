e1945ae461d0240fb845738e78bf7b1ccb37f21f
Vivek Nair <viveknair297@gmail.com>
2022-03-28 15:19:46 +1100

    delete any resource by just removing the code or commenting.

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ provider "aws" {
 }
 
 # resource "<provider>_<resource_type>" "name" { config option ... key = "value" key2 = "another value"}
-resource "aws_instance" "my-first-server" {
-  ami           = "ami-0b7dcd6e6fd797935"
-  instance_type = "t2.micro"
+# resource "aws_instance" "my-first-server" {
+#   ami           = "ami-0b7dcd6e6fd797935"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "ubuntu"
-  }
-}
+#   tags = {
+#     Name = "ubuntu"
+#   }
+# }
