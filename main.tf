12470c5163e0dd908cb3d8d20d686766a39bfee8
Vivek Nair <viveknair297@gmail.com>
2022-03-28 13:15:04 +1100

    Added main and terraform.lock.hcl file which contains code for simple deployment of one ec2 ubuntu server in free-tier.

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+provider "aws" {
+  region     = "ap-southeast-2"
+  profile = "default"
+}
+
+# resource "<provider>_<resource_type>" "name" { config option ... key = "value" key2 = "another value"}
+resource "aws_instance" "my-first-server" {
+  ami           = "ami-0b7dcd6e6fd797935"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my-first-server"
+  }
+}
