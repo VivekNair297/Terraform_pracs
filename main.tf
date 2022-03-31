61afed414be09c8e304d32feb8d7fc1fdc2e44f6
Vivek Nair <viveknair297@gmail.com>
2022-03-31 13:05:35 +1100

    Learning and applying how to use referencing resource in terraform.

--- main.tf
+++ main.tf
@@ -10,6 +10,22 @@ provider "aws" {
   profile = "default"
 }
 
+resource "aws_vpc" "first-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
 # resource "<provider>_<resource_type>" "name" { config option ... key = "value" key2 = "another value"}
 # resource "aws_instance" "my-first-server" {
 #   ami           = "ami-0b7dcd6e6fd797935"
